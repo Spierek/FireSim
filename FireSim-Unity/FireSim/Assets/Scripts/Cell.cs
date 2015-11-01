4eca7ce34f01b497f2c5bd69213c4f5767aae4a8
Luke Spierewka <spierek@gmail.com>
2015-11-01 22:24:36 +0100

    [+] Project setup [+] Initial world generation

---
+++ FireSim-Unity/FireSim/Assets/Scripts/Cell.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cell : MonoBehaviour {
+	#region Variables
+	#endregion
+
+	#region Monobehaviour
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+	#endregion
+
+	#region Methods
+	#endregion
+}
---
+++ FireSim-Unity/FireSim/Assets/Scripts/WorldGenerator.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WorldGenerator : MonoBehaviour {
+	#region Variables
+	public GameObject	cellPrefab;
+
+	[Header("World Params")]
+	public int			sizeX;
+	public int			sizeY;
+
+	//[HideInInspector]
+	public List<List<Cell>> cells = new List<List<Cell>>();
+	#endregion
+
+	#region Monobehaviour
+	void Awake () {
+		Generate();
+	}
+	
+	void Update () {
+	
+	}
+	#endregion
+
+	#region Methods
+	private void Generate() {
+		GameObject go;
+		for (int y = 0; y < sizeY; ++y) {
+			cells.Add(new List<Cell>());
+
+			for (int x = 0; x < sizeX; ++x) {
+				go = Instantiate(cellPrefab);
+
+				go.transform.parent = transform;
+				go.transform.localPosition = new Vector3(x - sizeX / 2 + 1, 0, -(y - sizeY / 2 + 1));
+				go.name = "Cell(" + x + "," + y + ")";
+
+				cells[y].Add(go.GetComponent<Cell>());
+			}
+		}
+	}
+	#endregion
+}
